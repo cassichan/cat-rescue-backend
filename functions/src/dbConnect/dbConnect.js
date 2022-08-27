fc049df11a31a1d849f72516fa33ebd1086dd30b
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-27 17:55:32 -0400

    added async await to get and post routers

--- functions/src/dbConnect/dbConnect.js
+++ functions/src/dbConnect/dbConnect.js
@@ -1,9 +1,14 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+// import { MongoClient, ServerApiVersion } from "mongodb";
+import {MongoClient} from "mongodb"
 
 import { uri } from "./credentials.js";
 
-export const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// export const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+const client = new MongoClient(uri)
+const db = client.db("animals")
+
+export default db;
--- functions/src/routers/routers.js
+++ functions/src/routers/routers.js
@@ -1,102 +1,71 @@
-import { client } from "../dbConnect/dbConnect.js";
+// import { client } from "../dbConnect/dbConnect.js";
+import db from "../dbConnect/dbConnect.js";
 
-export const getCats = (req, res) => {
-    client.connect((err) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      const collection = client
-        .db("animals")
-        .collection("real-cats");
-      collection.find().toArray((err, result) => {
-        if (err) res.status(500).send(err);
-        if (result) res.json(result);
-        client.close();
-      });
-    });
-  };
+const cats = db.collection("real-cats");
+const dogs = db.collection("real-dogs");
 
-  //Update favorite
-  // export const updateCat = (req, res) => {
-  //   const {_id} = req.params;
-  //   const {favorite} = req.body;
-  //   const collection = client
-  //   .db("animals")
-  //   .collection("real-cats")
-  //   collection.findOneAndUpdate(_id, favorite)
-  // }
+export const getCats = async (req, res) => {
+  const allCats = await cats
+    .find()
+    .toArray()
+    .catch((err) => res.status(500).send(err));
+  res.json(allCats);
+};
 
+export const getDogs = async (req, res) => {
+  const allDogs = await dogs
+    .find()
+    .toArray()
+    .catch((err) => res.status(500).send(err));
+  res.json(allDogs);
+};
 
-  //Update favorite
-  // export const updateDog = (req, res) => {
-  //   const {_id} = req.params;
-  //   const {favorite} = req.body;
-  //   const collection = client
-  //   .db("animals")
-  //   .collection("real-dogs")
-  //   collection.findOneAndUpdate(_id, favorite)
-  // }
+export const addCat = async (req, res) => {
+  const newCat = req.body;
+  await cats.insertOne(newCat);
+  const allCats = await cats
+    .find()
+    .toArray()
+    .catch((err) => res.status(500).send(err));
+  res.json(allCats);
+};
 
-  export const getDogs = (req, res) => {
-    client.connect((err) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      const collection = client
-        .db("animals")
-        .collection("real-dogs");
-      collection.find().toArray((err, result) => {
-        if (err) res.status(500).send(err);
-        if (result) res.json(result);
-        client.close();
-      });
-    });
-  };
+export const addDog = async (req, res) => {
+  const newDog = req.body;
+  await dogs.insertOne(newDog);
+  const allDogs = await dogs
+    .find()
+    .toArray()
+    .catch((err) => res.status(500).send(err));
+  res.json(allDogs);
+};
 
-  export const addCat = (req, res) => {
-    client.connect((err) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      const newCat = req.body;
-      const collection = client
-        .db("animals")
-        .collection("real-cats");
-      collection.insertOne(newCat, (err, result) => {
-        if (err) res.status(500).send(err);
-        if (result) res.json(result);
-        client.close();
-      });
-    });
-  };
+//Update favorite
+// export const updateCat = (req, res) => {
+//   const {_id} = req.params;
+//   const {favorite} = req.body;
+//   const collection = client
+//   .db("animals")
+//   .collection("real-cats")
+//   collection.findOneAndUpdate(_id, favorite)
+// }
 
-  export const addDog = (req, res) => {
-    client.connect((err) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      const newDog = req.body;
-      const collection = client
-        .db("animals")
-        .collection("real-dogs");
-      collection.insertOne(newDog, (err, result) => {
-        if (err) res.status(500).send(err);
-        if (result) res.json(result);
-        client.close();
-      });
-    });
-  };
+//Update favorite
+// export const updateDog = (req, res) => {
+//   const {_id} = req.params;
+//   const {favorite} = req.body;
+//   const collection = client
+//   .db("animals")
+//   .collection("real-dogs")
+//   collection.findOneAndUpdate(_id, favorite)
+// }
 
 export function deleteCat(req, res) {
-    const {_id} = req.params;
-    res.status(203).send("Cat has been deleted from system.")
+  const { _id } = req.params;
+  res.status(203).send("Cat has been deleted from system.");
 }
 
 export function deleteDog(req, res) {
-  const {_id} = req.params;
-  res.status(203).send("Dog has been deleted from system.")
+  const { _id } = req.params;
+  res.status(203).send("Dog has been deleted from system.");
 }
