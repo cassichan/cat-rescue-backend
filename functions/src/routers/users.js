a58deccd4e70e8cd03c490dfe8150be94e7f6a04
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-11-06 18:07:09 -0500

    removed comments and sent email in response

--- functions/src/routers/users.js
+++ functions/src/routers/users.js
@@ -2,12 +2,11 @@ import jwt from "jsonwebtoken";
 import db from "../dbConnect/dbConnect.js";
 import { secretKey } from "../dbConnect/credentials.js";
 import crypto from "crypto";
-// import { user } from "firebase-functions/v1/auth";
 import { ObjectId } from "mongodb";
 
 export async function createUser(req, res) {
   let { email, password } = req.body;
-  email = email.toLowerCase();
+  email = email.toLowerCase()
   let encryptedPassword = crypto
     .createHash("md5")
     .update(password)
@@ -17,9 +16,9 @@ export async function createUser(req, res) {
     .insertOne({ email, password: encryptedPassword, favoritePets: [] })
     .catch((err) => res.status(500).send(err));
   const token = jwt.sign({ email, id: user.id }, secretKey);
-  res.send({ token });
+  res.send({ token, email });
 }
-
+// }
 export async function loginUser(req, res) {
   let { email, password } = req.body;
   email = email.toLowerCase();
@@ -30,16 +29,14 @@ export async function loginUser(req, res) {
   const user = await db
     .collection("users")
     .findOne({ email: email, password: encryptedPassword });
-  // .toArray();
-
   if (user) {
     const token = jwt.sign({ email: user.email, id: user._id }, secretKey, {
       expiresIn: "1h",
     });
-    res.send({ token });
-    console.log(token);
+    res.send({ token, email });
   } else {
-    res.status(401).send("Wrong username and password");
+    res.status(401).send("Invalid username or password");
+    return;
   }
 }
 
@@ -47,9 +44,12 @@ export async function updateFavoriteList(req, res) {
   const token = req.headers.authorization;
   try {
     const validToken = jwt.verify(token, secretKey);
+    // console.log(validToken);
     let id = new ObjectId(validToken.id);
     const user = await db.collection("users").findOne({ _id: id });
-    // console.log(user)
+    if (user.favoritePets.includes(cats.id) === false) {
+      user.favoritePets.push();
+    }
   } catch (err) {
     res.status(500).send(err);
   }
