2733d6bf22917ef2f436b721123e236a556facdf
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-11-06 20:11:19 -0500

    email must be unique for sign up

--- functions/src/routers/users.js
+++ functions/src/routers/users.js
@@ -6,19 +6,33 @@ import { ObjectId } from "mongodb";
 
 export async function createUser(req, res) {
   let { email, password } = req.body;
-  email = email.toLowerCase()
+  email = email.toLowerCase();
   let encryptedPassword = crypto
     .createHash("md5")
     .update(password)
     .digest("hex");
-  const user = await db
+  const matchingUser = await db
     .collection("users")
-    .insertOne({ email, password: encryptedPassword, favoritePets: [] })
-    .catch((err) => res.status(500).send(err));
-  const token = jwt.sign({ email, id: user.id }, secretKey);
-  res.send({ token, email });
+    .find({ email: email })
+    .toArray();
+  console.log({ matchingUser: matchingUser });
+
+  if (matchingUser.length) {
+    res.status(401).send("Email already in use");
+    return;
+  } else {
+    const user = await db
+      .collection("users")
+      .insertOne({ email, password: encryptedPassword, favoritePets: [] });
+    if (user) {
+      const token = jwt.sign({ email: user.email, id: user._id }, secretKey, {
+        expiresIn: "1h",
+      });
+      res.send({ token, email });
+    }
+  }
 }
-// }
+
 export async function loginUser(req, res) {
   let { email, password } = req.body;
   email = email.toLowerCase();
@@ -30,7 +44,7 @@ export async function loginUser(req, res) {
     .collection("users")
     .findOne({ email: email, password: encryptedPassword });
   if (user) {
-    const token = jwt.sign({ email: user.email, id: user._id }, secretKey, {
+    const token = jwt.sign({ email: user.email, id: user.id }, secretKey, {
       expiresIn: "1h",
     });
     res.send({ token, email });
